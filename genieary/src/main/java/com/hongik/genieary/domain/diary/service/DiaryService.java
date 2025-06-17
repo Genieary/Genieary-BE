6f18381fefc43cf66a74c3b5e3dddcf634cfa929
OneE <bestoneeee@naver.com>
2025-06-17 16:48:50 +0900

    feat: 일기 조회 api 구현 및 일기 작성과 수정 시 diaryDate항목 dto에 추가 (#20)

--- genieary/src/main/java/com/hongik/genieary/domain/diary/service/DiaryService.java
+++ genieary/src/main/java/com/hongik/genieary/domain/diary/service/DiaryService.java
@@ -7,8 +7,8 @@ import com.hongik.genieary.domain.user.entity.User;
 
 public interface DiaryService {
 
-    DiaryResponseDto.DiaryResultDto createDiary(CustomUserDetails user, DiaryRequestDto.CreateDto requestDto);
-    DiaryResponseDto.DiaryResultDto updateDiary(Long diaryId, User user, DiaryRequestDto.UpdateDto dto);
+    DiaryResponseDto.DiaryResultDto createDiary(CustomUserDetails user, DiaryRequestDto.DiaryCreateDto requestDto);
+    DiaryResponseDto.DiaryResultDto updateDiary(Long diaryId, User user, DiaryRequestDto.DiaryUpdateDto dto);
     void deleteDiary(User user, Long diaryId);
-
+    DiaryResponseDto.DiaryResultDto getDiary(Long diaryId, User user);
 }
