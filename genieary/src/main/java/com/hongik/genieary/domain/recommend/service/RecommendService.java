fdbd8d960bb65b17f7cc1e38ba51e04bd927b4ca
OneE <bestoneeee@naver.com>
2025-10-11 16:29:34 +0900

    feat: 날짜에 해당하는 추천받은 선물 목록 조회 기능 추가

--- genieary/src/main/java/com/hongik/genieary/domain/recommend/service/RecommendService.java
+++ genieary/src/main/java/com/hongik/genieary/domain/recommend/service/RecommendService.java
@@ -3,6 +3,7 @@ package com.hongik.genieary.domain.recommend.service;
 import com.hongik.genieary.domain.recommend.Category;
 import com.hongik.genieary.domain.recommend.dto.RecommendResponseDto;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface RecommendService {
@@ -11,4 +12,6 @@ public interface RecommendService {
     RecommendResponseDto.LikeResultDto togleLikeGift(Long userId, Long recommendId);
 
     RecommendResponseDto.HateResultDto togleHateGift(Long userId, Long recommendId);
+
+    List<RecommendResponseDto.GiftResultDto> getRecommendGifts(Long userId, LocalDate date);
 }
