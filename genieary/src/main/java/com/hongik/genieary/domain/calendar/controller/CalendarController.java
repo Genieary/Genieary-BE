d879f1bf30b791e94475d0890b1c898d37ddd520
OneE <bestoneeee@naver.com>
2025-10-12 01:15:17 +0900

    feat: 캘린더 조회 기능

--- genieary/src/main/java/com/hongik/genieary/domain/calendar/controller/CalendarController.java
+++ genieary/src/main/java/com/hongik/genieary/domain/calendar/controller/CalendarController.java
@@ -5,16 +5,14 @@ import com.hongik.genieary.common.response.ApiResponse;
 import com.hongik.genieary.common.status.SuccessStatus;
 import com.hongik.genieary.common.swagger.DiaryNotFoundApiResponse;
 import com.hongik.genieary.common.swagger.SuccessSummaryResponse;
+import com.hongik.genieary.domain.calendar.dto.CalendarResponseDto;
 import com.hongik.genieary.domain.calendar.service.CalendarService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Calendar API", description = "한 달 요약 API")
 @RestController
@@ -34,6 +32,18 @@ public class CalendarController {
     public ResponseEntity<ApiResponse> getDiarySummary(@AuthenticationPrincipal CustomUserDetails userDetails, @PathVariable Long calendarId) {
         String summary = calendarService.getSummary(userDetails.getUser().getId(), calendarId);
         return ApiResponse.onSuccess(SuccessStatus._OK, summary);
+    }
+
+    @GetMapping
+    @Operation(
+            summary = "캘린더 조회",
+            description = "년도와 달에 해당하는 캘린더를 조회하는 api입니다.")
+    public ResponseEntity<ApiResponse> getCalendar(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @RequestParam int year,
+            @RequestParam int month) {
+        CalendarResponseDto.CalendarResultDto dto  = calendarService.getCalendar(userDetails.getUser().getId(), year, month);
+        return ApiResponse.onSuccess(SuccessStatus._OK, dto);
     }
 
 }
